17559041fdd6311999de3a3ba4f1dc3e88a683d7
Johannes Scribante <johannesscr@Johanness-MacBook-2.local>
2018-08-02 23:32:57 +0200

    patch - clean up

--- includes/sendgrid.integration.js
+++ includes/sendgrid.integration.js
@@ -3,8 +3,6 @@ const path = require('path');
 
 const sgMail = require('@sendgrid/mail');
 
-// let template = require('./../templates/test.html');
-
 const test_mail = (req, res) => {
     sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
@@ -40,30 +38,7 @@ const test_mail = (req, res) => {
     res.send('logged template');
 };
 
-// const test_mail = (req, res) => {
-//     const sgMail = require('@sendgrid/mail');
-//     sgMail.setApiKey(process.env.SENDGRID_API_KEY);
-//     const msg = {
-//         to: 'test@example.com',
-//         from: 'test@example.com',
-//         subject: 'Sending with SendGrid is Fun',
-//         text: 'and easy to do anywhere, even with Node.js',
-//         html: '<strong>and easy to do anywhere, even with Node.js</strong>',
-//     };
-//     sgMail.send(msg);
-//     res.send('logged template');
-// };
-
 module.exports = {
     sgMail,
     test_mail
 };
-
-// const msg = {
-//     to: 'test@example.com',
-//     from: 'test@example.com',
-//     subject: 'Sending with SendGrid is Fun',
-//     text: 'and easy to do anywhere, even with Node.js',
-//     html: '<strong>and easy to do anywhere, even with Node.js</strong>',
-// };
-// sgMail.send(msg);
